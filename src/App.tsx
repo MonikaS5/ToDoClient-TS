7c8fccbbbe59ac9d7047d9cc1003befad7ad6a5b
MonikaS5 <monika.bhagade@gmail.com>
2024-10-03 12:50:35 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+//App.js
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './components/Todo';
+import logo from './logo.png';
+import './App.css';
+ 
+function App() {
+ 
+  return (
+    <div>
+      <div className='bg-light p-3 mb-5'>
+        <h1 className=' text-center'> <img src={logo} className="App-logo me-3" alt="logo" />
+          To-Do List</h1>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Todo />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
